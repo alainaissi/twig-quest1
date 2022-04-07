8061ea387304ada5cbeee27661d97763448465fd
alainaissi <alain-aissi_student2022@wilder.school>
2022-04-07 10:52:06 +0200

    work is done

---
+++ public/products.php
@@ -0,0 +1,10 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+// echo $twig->render('products.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', array('products' => $products));
